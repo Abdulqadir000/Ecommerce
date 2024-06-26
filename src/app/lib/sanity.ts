b7023dc220ec0000fe38a7fb6c343ca94c637e93
Abdulqadir000 <154420547+Abdulqadir000@users.noreply.github.com>
2024-06-27 02:47:25 +0500

    fifth fix data sanity problem

--- src/app/lib/sanity.ts
+++ src/app/lib/sanity.ts
@@ -1,15 +1,19 @@
-import  imageUrlBuilder  from "@sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url";
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-    projectId: "lt8jbu9k",
-    dataset: "production",
-    apiVersion: "2022-03-25",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+
+    // projectId: "lt8jbu9k",
+    // dataset: "production",
+    // apiVersion: "2022-03-25",
     useCdn: false,
 })
 
 const builder = imageUrlBuilder(client);
 
-export function urlFor(source:any){
+export function urlFor(source: any) {
     return builder.image(source);
 }
